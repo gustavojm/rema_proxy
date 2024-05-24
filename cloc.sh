af47220cc987a4c321be789cc14cee2f9f781e58
Gustavo Malano <gmalano@na-sa.com.ar>
2024-05-24 12:49:33 -0300

    added code line counter script

---
+++ cloc.sh
@@ -0,0 +1,1 @@
+~/go/bin/scc -v src/ inc/ wwwroot/ CMakeLists.txt wwwroot/js/misc.js wwwroot/js/ajaxqueue.js wwwroot/css/styles.css --exclude-dir inc/rapidxml-1.13/ --exclude-dir wwwroot/js/ --exclude-dir wwwroot/css --exclude-file json.h
